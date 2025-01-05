39f6c61b0e3fc053a257625f7a845c7f88b0a28a
KirillSmirnov236 <58817071+KirillSmirnov236@users.noreply.github.com>
2025-01-05 21:22:35 +0300

    unit class

---
+++ Unit.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+    public class Unit
+    {
+        private float _health;
+        private float _armor;
+        public string Name { get; }
+        public float Health => _health;
+        public int Damage;
+        public float Armor => _armor;
+
+        public Unit() : this("Unknown Unit")
+        {
+            
+        }
+
+        public Unit(string name)
+        {
+            Name = name;
+            Damage = 5;
+            _armor = 0.6f;
+        }
+        public float GetRealHealth()
+        {
+            return Health * (1f + Armor);
+        }
+        public bool SetDamage(float value)
+        {
+            _health = Health - value * Armor;
+            
+            if (Health <= 0f)
+            {
+                return true;
+            }
+            else 
+            { 
+                return false; 
+            }
+        }
+    }
+}
